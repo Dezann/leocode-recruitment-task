bb0cec81e031fe75c8523e9fa910fed19ecc715d
Bartosz Kępka <bartoszkepka1999@gmail.com>
2021-11-12 16:41:41 +0100

    Initial commit

---
+++ src/Views/SearchPage/SearchPage.styled.tsx
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components'
+
+export const Container = styled.div`
+    justify-content:center;
+    align-items:center;
+    flex: 1;
+    text-align: center;
+    display: flex;
+    height: 100vh;
+`
+
+export const Title = styled.h1`
+    
+`
---
+++ src/Views/SearchPage/SearchPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Title } from './SearchPage.styled'
+
+export default function SearchPage() {
+    return (
+        <Container>
+            <Title>Users List</Title>
+        </Container>
+    )
+}
